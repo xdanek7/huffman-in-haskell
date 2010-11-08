dc2447d268323960b61a4e91e0537ab28cd7b3bc
xdanek7 <dnk@mail.muni.cz>
2010-11-08 10:56:27 +0100

    Začínám dokumentovat.
    Přejmenoval jsem některé funkce v Bity.hs, takže jsem vše rozbil...

--- Bity.hs
+++ Bity.hs
@@ -8,21 +8,34 @@ import Data.Word
 data Bit = H | L
   deriving (Eq, Read, Show)
 
+{-|
+  Funkce 'rozdel_bity' rozdělí seznam hodnot typu Bit na seznam seznamů Bitů, každý o stejné délce 8, tedy počet bitů v jednom bajtu.
+  Jedná se o předpřipravenou funkci 'rozděl_bity'
+-}
+rozdel_bity_po_osmi = rozdel_bity 8
 
-
-
-rozdelBityPoOsmi :: [Bit] -> [[Bit]]
-rozdelBityPoOsmi [] = []
-rozdelBityPoOsmi x = takeBitsWithPadding 8 x : rozdelBityPoOsmi (drop 8 x)
+{-|
+  Funkce 'rozdel_bity' rozdělí seznam hodnot typu Bit na seznam seznamů Bitů, každý o stejné délce 'n'.
+  Pokud je seznam 'x' prázdný je výstupem vždy prázdný seznam.
+  Pokud délka seznamu není násobkem n, bude poslední n-tice zarovnána příslušným počtem hodnot L
+-}
+rozdel_bity :: Integer     -- ^ n: Délka každé n-tice
+            -> [Bit]       -- ^ x: Vstupní seznam
+            -> [[Bit]]     -- ^ Výstup: prázdný seznam, nebo seznam seznamů Bitů, každý o stejné délce 'n'
+rozdel_bity _ [] = []
+rozdel_bity n x = takeBitsWithPadding n x : rozdel_bity (drop n x)
   where
     takeBitsWithPadding :: Integer -> [Bit] -> [Bit]
     takeBitsWithPadding 0 _ = []
     takeBitsWithPadding n [] = L : takeBitsWithPadding (n-1) []
     takeBitsWithPadding n (x:xs) = x : takeBitsWithPadding (n-1) xs
-
-prepracujBityNaBajty :: [[Bit]] -> [Data.Word.Word8]
-prepracujBityNaBajty [] = []
-prepracujBityNaBajty x = map spoj x
+{-|
+  Inverzní funkce 
+-}
+prepracuj_bity_na_bajty :: [[Bit]]              -- ^ 
+                        -> [Data.Word.Word8]    -- ^
+prepracuj_bity_na_bajty [] = []
+prepracuj_bity_na_bajty x = map spoj x
   where
     spoj :: [Bit] -> Data.Word.Word8
     spoj [] = 0      
@@ -30,11 +43,17 @@ prepracujBityNaBajty x = map spoj x
       | x == H = bit (length xs) .|. spoj xs
       | x == L = spoj xs
 
-
+{-|
+  Inverzní funkce k 'prepracuj_bity_na_bajty'
+-}
 prepracujBajtyNaBity :: [Data.Word.Word8] -> [[Bit]]
 prepracujBajtyNaBity = map bajtNaBity
 
+{-|
 
+-}
+--new_Bajt
+--type bajt = [Bit] -- ^ ! právě 8prvkový seznam
 bajtNaBity :: Data.Word.Word8 -> [Bit]
 bajtNaBity i = map (f i) bb
   where
